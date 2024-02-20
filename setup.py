23c20a3ce553d9b1fb0d57baf4676088c58569cd
Leonard Austin <leonardaustin@users.noreply.github.com>
2024-02-20 15:25:02 +0000

    update licence

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
    ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: Apache License 2.0",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',  # Or your minimum Python requirement
