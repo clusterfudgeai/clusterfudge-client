6b76c5f456e319f9c8e176233f44cf2dadc76244
George Thomas <georgetamayothomas@gmail.com>
2024-02-28 12:15:07 +0000

    Bump to version 0.0.5

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='clusterfudge',  # Name used on PyPI (make it unique)
-    version='0.0.4',
-    description='Clusterfudge Python client library.',
-    long_description=open('README.md').read(), 
-    long_description_content_type='text/markdown',
-    author='Clusterfudge',
-    author_email='pip@clusterfudge.com',
-    url='https://github.com/clusterfudgeai/python', 
+    name="clusterfudge",  # Name used on PyPI (make it unique)
+    version="0.0.5",
+    description="Clusterfudge Python client library.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Clusterfudge",
+    author_email="pip@clusterfudge.com",
+    url="https://github.com/clusterfudgeai/python",
     packages=find_packages(),  # Finds packages automatically
-   install_requires=[
+    install_requires=[
         "dataclasses; python_version<'3.7'",
         "dataclasses-json",
         "grpcio",
         "protobuf",
-   ],
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',  # Or your minimum Python requirement
+    python_requires=">=3.6",  # Or your minimum Python requirement
 )
